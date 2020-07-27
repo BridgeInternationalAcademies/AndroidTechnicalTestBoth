24c1849808006010bdb316a63f6b2be885b2b32d
ARNAVbot <arnav.agarwal@ofbusiness.in>
2020-07-27 10:42:56 +0530

    migrate to android x

--- android-tech-test-java/app/src/main/java/com/bridge/androidtechnicaltest/db/AppDatabase.java
+++ android-tech-test-java/app/src/main/java/com/bridge/androidtechnicaltest/db/AppDatabase.java
@@ -1,7 +1,7 @@
 package com.bridge.androidtechnicaltest.db;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {
         Pupil.class
--- android-tech-test-java/app/src/main/java/com/bridge/androidtechnicaltest/ui/MainActivity.java
+++ android-tech-test-java/app/src/main/java/com/bridge/androidtechnicaltest/ui/MainActivity.java
@@ -1,25 +1,19 @@
 package com.bridge.androidtechnicaltest.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.bridge.androidtechnicaltest.App;
 import com.bridge.androidtechnicaltest.R;
 import com.bridge.androidtechnicaltest.db.PupilDao;
-import com.bridge.androidtechnicaltest.db.PupilList;
 import com.bridge.androidtechnicaltest.network.PupilService;
 
 import javax.inject.Inject;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableCompletableObserver;
-import io.reactivex.observers.DisposableSingleObserver;
-import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
